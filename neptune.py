407a3ccb0acd7b45b0990c0826bab4c2ff096d8a
opopiol <opopiol@edu.cdv.pl>
2021-06-24 11:49:59 +0200

    neptune

--- neptune.py
+++ neptune.py
@@ -1,3 +1,16 @@
 #pip install neptune-client
-import neptune.new as neptune
-run = neptune.init(project='opopiol/ML-project')
+#pip install --upgrade --quiet neptune-client
+#pip install neptune-notebooks
+run = neptune.init(project='opopiol/ML-project',
+                   api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI5YjIyYjU2ZS0wMTc5LTQ3NWUtOWZkZC05OTg2YzI1M2VkNDUifQ==') # your credentials
+
+run["JIRA"] = "NPT-952"
+run["parameters"] = {"learning_rate": 0.001,
+                     "optimizer": "Adam"}
+
+for epoch in range(100):
+   run["train/loss"].log(epoch * 0.4)
+run["eval/f1_score"] = 0.66
+
+from google.colab import drive
+drive.mount('/content/drive')
