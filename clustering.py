f892852952cee721d01fff64d6ab730461576a24
lucjaesterastaniek <lucjaesterastaniek@gmail.com>
2021-06-29 13:18:00 +0200

    Modification: PCA and plots Expansion: scaling data, eda, imports

--- clustering.py
+++ clustering.py
@@ -1,11 +1,26 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.dummy import DummyClassifier
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.decomposition import PCA
-
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn import svm
+from imblearn.over_sampling import SMOTE
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier
+import io
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.manifold import TSNE
+from sklearn.metrics import confusion_matrix
 import neptune.new as neptune
 #run = neptune.init(project='opopiol/ML-project')
 run = neptune.init(project='opopiol/ML-project',
@@ -19,20 +34,102 @@ for epoch in range(100):
    run["train/loss"].log(epoch * 0.4)
 run["eval/f1_score"] = 0.66
 
+
+# Reducing DataFrame memory size
+# https://www.kaggle.com/arjanso/reducing-dataframe-memory-size-by-65?fbclid=IwAR3XPzjakD69RqAEKuAnTDUtfw3AeCAj19eyd6LfzVSwHHICNgxW-ptK-vs
+
+def reduce_mem_usage(props):
+    start_mem_usg = props.memory_usage().sum() / 1024 ** 2
+    print("Memory usage of properties dataframe is :", start_mem_usg, " MB")
+    NAlist = []  # Keeps track of columns that have missing values filled in.
+    for col in props.columns:
+        if props[col].dtype != object:  # Exclude strings
+
+            # Print current column type
+            # print("******************************")
+            # print("Column: ",col)
+            # print("dtype before: ",props[col].dtype)
+
+            # make variables for Int, max and min
+            IsInt = False
+            mx = props[col].max()
+            mn = props[col].min()
+
+            # Integer does not support NA, therefore, NA needs to be filled
+            if not np.isfinite(props[col]).all():
+                NAlist.append(col)
+                props[col].fillna(mn - 1, inplace=True)
+
+                # test if column can be converted to an integer
+            asint = props[col].fillna(0).astype(np.int64)
+            result = (props[col] - asint)
+            result = result.sum()
+            if result > -0.01 and result < 0.01:
+                IsInt = True
+
+            # Make Integer/unsigned Integer datatypes
+            if IsInt:
+                if mn >= 0:
+                    if mx < 255:
+                        props[col] = props[col].astype(np.uint8)
+                    elif mx < 65535:
+                        props[col] = props[col].astype(np.uint16)
+                    elif mx < 4294967295:
+                        props[col] = props[col].astype(np.uint32)
+                    else:
+                        props[col] = props[col].astype(np.uint64)
+                else:
+                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
+                        props[col] = props[col].astype(np.int8)
+                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
+                        props[col] = props[col].astype(np.int16)
+                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
+                        props[col] = props[col].astype(np.int32)
+                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
+                        props[col] = props[col].astype(np.int64)
+
+                        # Make float datatypes 32 bit
+            else:
+                props[col] = props[col].astype(np.float32)
+
+            # Print new column type
+            # print("dtype after: ",props[col].dtype)
+            # print("******************************")
+
+# Print final result
+print("___MEMORY USAGE AFTER COMPLETION:___")
+mem_usg = props.memory_usage().sum() / 1024 ** 2
+print("Memory usage is: ", mem_usg, " MB")
+print("This is ", 100 * mem_usg / start_mem_usg, "% of the initial size")
+return props
+
+
 from google.colab import drive
 drive.mount('/content/drive')
 
 #import train dataset
 train_path = '/content/drive/MyDrive/project/train_data.csv'
-X = pd.read_csv(train_path, header=None)
+train_data = pd.read_csv(train_path, header=None)
+
+train_data= reduce_mem_usage(train_data)
 
 #import labels
-labels = '/content/drive/MyDrive/project/train_labels.csv'
-y = pd.read_csv(labels, header=None,  names=['y'])
+labels_path = '/content/drive/MyDrive/project/train_labels.csv'
+labels = pd.read_csv(labels_path, header=None,  names=['y'])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, stratify=y)
+labels= reduce_mem_usage(labels)
 
-"""### EDA"""
+#import test dataset
+test_path = '/content/drive/MyDrive/project/test_data.csv'
+test_data = pd.read_csv(test_path, header=None)
+
+test_data= reduce_mem_usage(test_data)
+
+X, y = train_data, labels
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+
+#EDA
 
 y.value_counts()
 
@@ -40,39 +137,72 @@ y = y['y'].apply(lambda x: 0 if x == -1 else 1)
 
 y.value_counts()
 
-y_train = y_train['y'].apply(lambda x: 0 if x == -1 else 1)
-
 X_train.shape
 
 y_train.shape
 
 #scaling data
 
+#StandardScaler
 scaler = StandardScaler()
 
 X_train_scale = StandardScaler().fit_transform(X_train)
 y_train_scale = scaler.fit_transform(y_train)
 
+#MinMaxScaler
+minmaxscaler = MinMaxScaler()
+X_train_minmaxscaler = MinMaxScaler().fit_transform(X_train)
+
 X_train_scale.mean(axis=0)
 
 #X_train_scale has now unit variance and zero mean
+
+y_train = y_train['y'].apply(lambda x: 0 if x == -1 else 1)
+
+#final_pca_df = pd.DataFrame(data = X_pca)
+#final_pca = pd.concat([final_pca_df, y_train], axis = 1)
+#final_pca
+
+sns.set_palette('Set1')
 
 #PCA
 
 pca = PCA(n_components=2, whiten=True)
 X_pca = pca.fit_transform(X_train_scale)
 
-X_pca.shape
-
-X_pca
-
-#final_pca_df = pd.DataFrame(data = X_pca)
-#final_pca = pd.concat([final_pca_df, y_train], axis = 1)
-#final_pca
-
-
-sns.set_palette('Set1')
-
-plt.figure(figsize=(8,8))
+plt.figure(figsize=(6,6))
+plt.title('PCA(n_components=2)')
 sns.scatterplot(X_pca[:,0],
-           X_pca[:,1], hue=y_train);
+           X_pca[:,1],
+           s=30, hue=y_train);
+
+pca = PCA(n_components=0.99, whiten=True)
+X_pca = pca.fit_transform(X_train_scale)
+
+plt.figure(figsize=(6,6))
+plt.title('PCA(n_components=0.99)')
+sns.scatterplot(X_pca[:,0],
+           X_pca[:,1],
+           s=30, hue=y_train);
+
+pca = PCA()
+tsne = TSNE()
+
+def clustering_scatterplot(a, b):
+  """This function creates scatterplot with clusters from seaborn's library
+    :param a: n_components for PCA
+    :param b: n_components for TSNE
+    :return: sns.scatterplot(X_p[:,0], X_p[:,1], c=y): scatterplot with clusters
+    """
+  pipeline = Pipeline([('pca', PCA(n_components = a)), ('tsne', TSNE(n_components = b))])
+  X_p = pipeline.fit_transform(X)
+
+  sns.scatterplot(X_p[:,0], X_p[:,1], c=y)
+  plt.title(f'PCA(n_components={a})&TSNE(n_components={b})');
+
+clustering_scatterplot(2, 2)
+
+#best for our project with n_components=0.99 for PCA and n_components=2 for TSNE
+clustering_scatterplot(0.99, 2)
+
+clustering_scatterplot(0.95, 2)
