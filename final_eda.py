847b0bb689a3169abe0c2128b68e26855bffb6a4
lucjaesterastaniek <lucjaesterastaniek@gmail.com>
2021-07-06 19:52:30 +0200

    closing in function - imports

--- final_eda.py
+++ final_eda.py
@@ -10,102 +10,37 @@ from sklearn.manifold import TSNE
 from sklearn.pipeline import Pipeline
 
 #----------------------------------------------------------------------------------
-# REDUCING DATAFRAME MEMORY SIZE
-# https://www.kaggle.com/arjanso/reducing-dataframe-memory-size-by-65?fbclid=IwAR3XPzjakD69RqAEKuAnTDUtfw3AeCAj19eyd6LfzVSwHHICNgxW-ptK-vs
-
-def reduce_mem_usage(props):
-    start_mem_usg = props.memory_usage().sum() / 1024 ** 2
-    print("Memory usage of properties dataframe is :", start_mem_usg, " MB")
-    NAlist = []  # Keeps track of columns that have missing values filled in.
-    for col in props.columns:
-        if props[col].dtype != object:  # Exclude strings
-
-            # Print current column type
-            # print("******************************")
-            # print("Column: ",col)
-            # print("dtype before: ",props[col].dtype)
-
-            # make variables for Int, max and min
-            IsInt = False
-            mx = props[col].max()
-            mn = props[col].min()
-
-            # Integer does not support NA, therefore, NA needs to be filled
-            if not np.isfinite(props[col]).all():
-                NAlist.append(col)
-                props[col].fillna(mn - 1, inplace=True)
-
-                # test if column can be converted to an integer
-            asint = props[col].fillna(0).astype(np.int64)
-            result = (props[col] - asint)
-            result = result.sum()
-            if result > -0.01 and result < 0.01:
-                IsInt = True
-
-            # Make Integer/unsigned Integer datatypes
-            if IsInt:
-                if mn >= 0:
-                    if mx < 255:
-                        props[col] = props[col].astype(np.uint8)
-                    elif mx < 65535:
-                        props[col] = props[col].astype(np.uint16)
-                    elif mx < 4294967295:
-                        props[col] = props[col].astype(np.uint32)
-                    else:
-                        props[col] = props[col].astype(np.uint64)
-                else:
-                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
-                        props[col] = props[col].astype(np.int8)
-                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
-                        props[col] = props[col].astype(np.int16)
-                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
-                        props[col] = props[col].astype(np.int32)
-                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
-                        props[col] = props[col].astype(np.int64)
-
-                        # Make float datatypes 32 bit
-            else:
-                props[col] = props[col].astype(np.float32)
-
-            # Print new column type
-            # print("dtype after: ",props[col].dtype)
-            # print("******************************")
-
-    # Print final result
-    print("___MEMORY USAGE AFTER COMPLETION:___")
-    mem_usg = props.memory_usage().sum() / 1024 ** 2
-    print("Memory usage is: ", mem_usg, " MB")
-    print("This is ", 100 * mem_usg / start_mem_usg, "% of the initial size")
-    return props
-
-
-#----------------------------------------------------------------------------------
 #DATA IMPORT
 
 from google.colab import drive
 drive.mount('/content/drive')
 
 #import train dataset
-train_path = '/content/drive/MyDrive/project/train_data.csv'
-train_data = pd.read_csv(train_path, header=None)
+def i_train_d():
+  train_path = '/content/drive/MyDrive/project/train_data.csv'
+  train_data = pd.read_csv(train_path, header=None)
 
-train_data= reduce_mem_usage(train_data)
+i_train_d()
 
 #import labels
-labels_path = '/content/drive/MyDrive/project/train_labels.csv'
-labels = pd.read_csv(labels_path, header=None,  names=['y'])
+def i_labels():
+  labels_path = '/content/drive/MyDrive/project/train_labels.csv'
+  labels = pd.read_csv(labels_path, header=None,  names=['y'])
 
-labels= reduce_mem_usage(labels)
+i_labels()
 
 #import test dataset
-test_path = '/content/drive/MyDrive/project/test_data.csv'
-test_data = pd.read_csv(test_path, header=None)
+def i_test_d():
+  test_path = '/content/drive/MyDrive/project/test_data.csv'
+  test_data = pd.read_csv(test_path, header=None)
 
-test_data= reduce_mem_usage(test_data)
+i_test_d()
 
-X, y = train_data, labels
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify = y, random_state=1)
+def x_y():
+  X, y = train_data, labels
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify = y, random_state=1)
 
+x_y()
 
 #----------------------------------------------------------------------------------
 #EDA
